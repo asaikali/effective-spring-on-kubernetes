9d570c3764fefc6e2468998f25b6fcf109a2df7d
Adib Saikali <asaikali@gmail.com>
2020-11-03 22:45:03 -0500

    Polish

--- fatjar-dockerfile/src/main/java/com/example/DemoApplication.java
+++ fatjar-dockerfile/src/main/java/com/example/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.example.fatjardockerfile;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FatjarDockerfileApplication {
+public class DemoApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(FatjarDockerfileApplication.class, args);
+    SpringApplication.run(DemoApplication.class, args);
   }
 
 }
--- fatjar-dockerfile/src/test/java/com/example/FatjarDockerfileApplicationTests.java
+++ fatjar-dockerfile/src/test/java/com/example/FatjarDockerfileApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.fatjardockerfile;
+package com.example;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
